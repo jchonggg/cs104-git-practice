d2c8124223c20ff748a47b102ef4935c00d63ffa
Jonathan Chong <jjchong@usc.edu>
2021-01-18 16:19:10 -0800

    Add hello_world.cpp with some code

---
+++ HelloWorld/HelloWorld/hello_world.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Jonathan Chong on 1/18/21.
+//  Copyright © 2021 Jonathan Chong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ hello_world.cpp
@@ -0,0 +1,13 @@
+//
+//  hello_world.cpp
+//  HelloWorld
+//
+//  Created by Jonathan Chong on 1/18/21.
+//  Copyright © 2021 Jonathan Chong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Hello, world!" << std::endl;
+}
